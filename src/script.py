c91d8153673648ec46c9bccc6b18e6d17e1dfb65
zhpmatrix <zhpmatrix@gmail.com>
2017-03-24 22:24:06 +0800

    fixed #10:using script to get it

--- src/script.py
+++ src/script.py
@@ -22,13 +22,12 @@ def gen_training_data(c_pq,c_pi,group_index,target_label,dirPath):
     	counter_grouped = grouped_pq.groupby(group_index[0])[group_index[1]]
     	print counter_grouped.count()
 	col_grouped = grouped_pq.groupby(group_index[0])
-	for key,items in col_grouped:
+        for key,items in col_grouped:
+		filePath = dirPath+str(key)+'.csv'
 		if target_label == 'product':
-			filePath = dirPath+str(key)+'.csv'
 			dropped_items = items.drop(group_index[0],axis=1)
 			training_data = dropped_items			
 		else:
-			filePath = dirPath+str(key)+'.csv'
 			dropped_items = items.drop(group_index[0],axis=1)
 			training_data = pd.merge(c_pi,dropped_items)
 		save_data(training_data,filePath)
@@ -72,8 +71,8 @@ c_pq.set_index(['product_id','product_date'])
 c_pi = pi.copy()
 c_pq['product_date'] = c_pq['product_date'].apply(lambda x:x[:7])
 
-#gen_training_data(c_pq,c_pi,['product_id','product_date'],'product',product_dirPath)
-#gen_training_data(c_pq,c_pi,['product_date','product_id'],'month',month_dirPath)
+gen_training_data(c_pq,c_pi,['product_id','product_date'],'product',product_dirPath)
+gen_training_data(c_pq,c_pi,['product_date','product_id'],'month',month_dirPath)
 
 product_not_exist = product_fig(product_dirPath,product_figPath)
 print 'product_not_exist: ',product_not_exist,'\nnumber of product not exist: ',len(product_not_exist)
