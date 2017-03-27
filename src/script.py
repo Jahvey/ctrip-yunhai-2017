7d10dc4e4faa3aa81262f627e46b30b8e0718976
zhpmatrix <zhpmatrix@gmail.com>
2017-03-27 23:28:34 +0800

    add rule method to fit and predit 23 product

--- src/script.py
+++ src/script.py
@@ -16,11 +16,55 @@ def show_grouped_data(grouped_data):
 		for subitem in items:
 			print subitem
 		print '-' * 30
+def get_product_set(data_grouped,cq_min_len,cq_max_len):
+	product_set = {}	
+	cq_min_len = 1
+	cq_max_len = 23
+	product_set = {}
+	for i in range(cq_min_len,cq_max_len+1):
+		product_set[i] = [key for key,items in data_grouped if len(items) == i]
+	#print product_set		
+	return product_set
+def saveData(data,filePath):
+	f = open(filePath,'w')
+	data = pd.DataFrame(data)
+	data.to_csv(f,index=False)
+	f.close()
+
+def mean_fitdata):
+	window_max = 4
+	errors = []
+	for window in range(2,window_max):
+		error = month_mean(data,window)
+		errors.append([window,error])
+	params = sorted(errors,key=lambda err:err[1],reverse=False)
+	best_window = params[0][0]
+	return best_window
+
+def mean_predict():
+	pass
+
+def month_mean(data,window):
+	data = np.array(data)
+	weight = np.array([[1.0/window]] * window)
+	valid_month = 3
+	test_data = data[:,0:data.shape[1]-valid_month]
+
+	errors = []
+	for i in range(0,valid_month):
+		cur_cq = np.dot(test_data[:,test_data.shape[1] - window:test_data.shape[1]],weight)
+		
+		test_data = np.column_stack((test_data,cur_cq))
+	return np.sum(data-test_data)**2
+
+def month_percent(data,window=3,percent=0.6):
+	pass
 
 def gen_training_data(c_pq,c_pi,group_index,target_label,dirPath):
 	grouped_pq = c_pq.groupby(group_index)['ciiquantity'].agg(np.sum).reset_index()
     	counter_grouped = grouped_pq.groupby(group_index[0])[group_index[1]]
-    	print counter_grouped.count()
+	#print counter_grouped.count()
+	product_set = get_product_set(counter_grouped,1,23)
 	# plot missing month ciiquantity for each product
 	# plt.show(counter_grouped.count().plot(kind='bar'))
 	# exit()
@@ -34,6 +78,7 @@ def gen_training_data(c_pq,c_pi,group_index,target_label,dirPath):
 			dropped_items = items.drop(group_index[0],axis=1)
 			training_data = pd.merge(c_pi,dropped_items)
 		save_data(training_data,filePath)
+	return product_set
 
 def save_data(data,filePath):
 	f = open(filePath,'w')
@@ -61,21 +106,36 @@ def product_fig(inPath,outPath):
 			print 'product {} isn\'t exist! '.format(i)
 			product_not_exist.append(i)
 	return product_not_exist
-		
-month_dirPath = '../training_data/month/'
-product_dirPath = '../training_data/product/'
-product_figPath = '../training_data/images/month_quantity/'
 
-pq = pd.read_csv('../product_data/product_quantity.txt')
-pi = pd.read_csv('../product_data/product_info.txt')
+if __name__ == '__main__':		
+	
+	cq_min_len = 22
+	cq_max_len = 23
 
-c_pq = pq.copy()
-c_pq.set_index(['product_id','product_date'])
-c_pi = pi.copy()
-c_pq['product_date'] = c_pq['product_date'].apply(lambda x:x[:7])
+	month_dirPath = '../training_data/month/'
+	product_dirPath = '../training_data/product/'
+	product_figPath = '../training_data/images/month_quantity/'
 
-gen_training_data(c_pq,c_pi,['product_id','product_date'],'product',product_dirPath)
-gen_training_data(c_pq,c_pi,['product_date','product_id'],'month',month_dirPath)
+	pq = pd.read_csv('../product_data/product_quantity.txt')
+	pi = pd.read_csv('../product_data/product_info.txt')
 
-product_not_exist = product_fig(product_dirPath,product_figPath)
-print 'product_not_exist: ',product_not_exist,'\nnumber of product not exist: ',len(product_not_exist)
+	c_pq = pq.copy()
+	c_pq.set_index(['product_id','product_date'])
+	c_pi = pi.copy()
+	c_pq['product_date'] = c_pq['product_date'].apply(lambda x:x[:7])
+	
+	#gen_training_data(c_pq,c_pi,['product_date','product_id'],'month',month_dirPath)
+
+	#product_not_exist = product_fig(product_dirPath,product_figPath)
+	#print 'product_not_exist: ',product_not_exist,'\nnumber of product not exist: ',len(product_not_exist)
+
+	product_set = gen_training_data(c_pq,c_pi,['product_id','product_date'],'product',product_dirPath)
+	for i in range(cq_min_len,cq_max_len + 1):
+		data = []
+		for name in product_set[i]:
+			tmp = pd.read_csv(product_dirPath+str(name)+'.csv')
+			data.append(tmp.loc[:,'ciiquantity'].tolist())
+		mean_fit(data)
+		exit()
+	
+
